1822cd573cd91560e2fcc0efe8c45890118eb584
neeraj <diddekuntaneeraj18@gmail.com>
2026-04-01 18:53:10 +0530

    name changed

--- basic/c00_helloworld/HelloWorld.java
+++ basic/c00_helloworld/HelloWorld.java
@@ -14,6 +14,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hola, Java!");
+     //   System.out.println("Hola, Java!");
+      System.out.println("Hello, Java!");
     }
 }
